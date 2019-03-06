4158b410438bf2081acd1c6e8b820a1292916758
Jason Zheng <zzheng3@stuy.edu>
2019-03-06 14:30:37 -0500

    Imported scanner.

--- USACO.java
+++ USACO.java
@@ -1,8 +1,9 @@
+import java.util.*;
 public class USACO {
   public static int bronze(String filename) throws FileNotFoundException{
     File text = new File(filename);
     Scanner inf = new Scanner(text);
-    
+    int R = Integer.parseInt()
   }
   public static int silver(String filename);
 }
