7c50076d5d5f3b78a20ca293f2064cb9604959f9
Jason Zheng <zzheng3@stuy.edu>
2019-03-06 14:33:34 -0500

    I have no idea what E and N are. LOL!

--- USACO.java
+++ USACO.java
@@ -2,8 +2,9 @@ import java.util.*;
 public class USACO {
   public static int bronze(String filename) throws FileNotFoundException{
     File text = new File(filename);
-    Scanner inf = new Scanner(text);
-    int R = Integer.parseInt()
+    Scanner inf = new Scanner(text); //Let's give meaning to the top four integers
+    int R = Integer.parseInt(text.next()); //Number of rows.
+    int C = Integer.parseInt(text.next()); //Number of columns.
   }
   public static int silver(String filename);
 }
