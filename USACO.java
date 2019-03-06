5127af44a01ae9543c27e5baae28e018adf3f997
Jason Zheng <zzheng3@stuy.edu>
2019-03-05 23:27:27 -0500

    Started work on constructor.

--- USACO.java
+++ USACO.java
@@ -1,5 +1,7 @@
 public class USACO {
-  public static int bronze(String filename) {
+  public static int bronze(String filename) throws FileNotFoundException{
+    File text = new File(filename);
+    Scanner inf = new Scanner(text);
     
   }
   public static int silver(String filename);
