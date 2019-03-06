b3efd3346ffe552eb8ac33435445637e7b4e8e07
Jason Zheng <zzheng3@stuy.edu>
2019-03-06 14:47:47 -0500

    Holy shit. I actually have no idea how to use scanner.

--- USACO.java
+++ USACO.java
@@ -5,6 +5,17 @@ public class USACO {
     Scanner inf = new Scanner(text); //Let's give meaning to the top four integers
     int R = Integer.parseInt(text.next()); //Number of rows.
     int C = Integer.parseInt(text.next()); //Number of columns.
+    int E = Integer.parseInt(text.next()); //The elevation.
+    int N = Integer.parseInt(text.next()); //Number of stomping instructions.
+    int[][] lake = int[R][C]; //Use 2D array to represent the lake.
+    Scanner brave = new Scanner(text); //Need a new scanner.
+    brave.nextLine(); //Move on to the elevation values.
+    for (int i = 0; i < R; i = i + 1) { //Add the elevation values.
+      for (int j = 0; j < C; j = j + 1) {
+        lake[i][j] = Integer.parseInt(text.next());
+      }
+      brave.nextLine();
+    }
   }
   public static int silver(String filename);
 }
