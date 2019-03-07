bb168d379a937c6dffa900758b4100ed013a33cd
Jason Zheng <zzheng3@stuy.edu>
2019-03-07 14:28:11 -0500

    Changed loop.

--- USACO.java
+++ USACO.java
@@ -5,18 +5,18 @@ public class USACO {
     File text = new File(filename);
     Scanner inf = new Scanner(text); //Let's give meaning to the top four integers
     String[] integers = inf.nextLine().split(""); //Splits the first line at spaces.
-    int R = Integer.parseInt(text.next()); //Number of rows.
-    int C = Integer.parseInt(text.next()); //Number of columns.
-    int E = Integer.parseInt(text.next()); //The elevation.
-    int N = Integer.parseInt(text.next()); //Number of stomping instructions.
+    int R = Integer.parseInt(integers[0]); //Number of rows.
+    int C = Integer.parseInt(integers[1]); //Number of columns.
+    int E = Integer.parseInt(integers[2]); //The elevation.
+    int N = Integer.parseInt(integers[3]); //Number of stomping instructions.
     int[][] lake = int[R][C]; //Use 2D array to represent the lake.
     Scanner brave = new Scanner(text); //Need a new scanner.
     brave.nextLine(); //Move on to the elevation values.
     for (int i = 0; i < R; i = i + 1) { //Add the elevation values.
+      String[] row = inf.nextLine().split(""); //Time to make the rows.
       for (int j = 0; j < C; j = j + 1) {
-        lake[i][j] = Integer.parseInt(text.next());
+        lake[i][j] = Integer.parseInt(row[j]); //Fill in the values.
       }
-      brave.nextLine();
     }
   }
   public static int silver(String filename);
